b566dfa901127ee35ba08fcaea2f08cbc111c280
YASH\dharmendra.biswal <dharmendra.biswal@yash.com>
2019-02-27 17:40:11 +0530

    Commit-1

---
+++ Springbootwithjwt/src/main/java/com/boot/Springbootwithjwt/exception/AppException.java
@@ -0,0 +1,17 @@
+package com.boot.Springbootwithjwt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class AppException extends RuntimeException{
+	
+	public AppException(String msg){
+		super(msg);
+	}
+	
+	 public AppException(String message, Throwable cause) {
+	        super(message, cause);
+	    }
+
+}
